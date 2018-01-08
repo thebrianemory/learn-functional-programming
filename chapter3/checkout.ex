b6a60ecee42f7eb0ba10a9e6325c61b85b75abeb
thebrianemory <the@brianemory.com>
2018-01-07 18:26:25 -0800

    Update modules

--- chapter3/checkout.ex
+++ chapter3/checkout.ex
@@ -1,3 +1,5 @@
 defmodule Checkout do
-  def total_cost(price, quantity \\ 10), do: price * quantity
+  def total_cost(price, tax_rate) when price >= 0 and tax_rate >= 0 do
+    price * (tax_rate + 1)
+  end
 end
--- chapter3/number_compare.ex
+++ chapter3/number_compare.ex
@@ -1,8 +1,5 @@
 defmodule NumberCompare do
-  def greater(number, other_number) do
-    check(number >= other_number, number, other_number)
-  end
+  def greater(number, other_number) when number >= other_number, do: number
 
-  defp check(true, number, _), do: number
-  defp check(false, _, other_number), do: other_number
+  def greater(_, other_number), do: other_number
 end
