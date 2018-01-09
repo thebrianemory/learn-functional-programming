0262adfd47e46486f59f61548b299e2503cf4735
thebrianemory <the@brianemory.com>
2018-01-08 20:21:29 -0800

    Refactor ability_modifiers

--- chapter3/ability_modifiers.exs
+++ chapter3/ability_modifiers.exs
@@ -1,4 +1,11 @@
 user_input = IO.gets "Write your ability score:\n"
-{ability_score, _} = Integer.parse(user_input)
-ability_modifier = (ability_score - 10) /2
-IO.puts "Your ability modifier is #{ability_modifier}"
+
+result = case Integer.parse(user_input) do
+  :error ->
+    "Invalid ability score: #{user_input}"
+  {ability_score, _} when ability_score >= 0 ->
+    ability_modifier = (ability_score -10) / 2
+    "Your ability modifier is #{ability_modifier}"
+end
+
+IO.puts result
