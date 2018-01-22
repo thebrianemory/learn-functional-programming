cc40ff315428b410fdf7b179e5b8bf8ff033c5a6
thebrianemory <the@brianemory.com>
2018-01-22 14:18:45 -0800

    Add chapter 4 stuff

---
+++ chapter4/enchanter_shop.ex
@@ -0,0 +1,28 @@
+defmodule EnchanterShop do
+  def test_data do
+    [
+      %{title: "Longsword", price: 50, magic: false},
+      %{title: "Healing potion", price: 60, magic: true},
+      %{title: "Rope", price: 10, magic: false},
+      %{title: "Dragon's Spear", price: 100, magic: true}
+    ]
+  end
+
+  @enchanter_name "Edwin"
+
+  def enchant_for_sale([]), do: []
+
+  def enchant_for_sale([item = %{magic: true} | incoming_items]) do
+    [item | enchant_for_sale(incoming_items)]
+  end
+
+  def enchant_for_sale([item | incoming_items]) do
+    new_item = %{
+      title: "#{@enchanter_name}'s #{item.title}",
+      price: item.price * 3,
+      magic: true
+    }
+
+    [new_item | enchant_for_sale(incoming_items)]
+  end
+end
