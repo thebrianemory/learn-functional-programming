14540ddf4db784934003f9c2827a7e7c5fb96608
thebrianemory <the@brianemory.com>
2018-01-27 20:51:11 -0800

    Update to include hero_choice

--- chapter6/dungeon_crawl/lib/dungeon_crawl/cli/main.ex
+++ chapter6/dungeon_crawl/lib/dungeon_crawl/cli/main.ex
@@ -3,11 +3,17 @@ defmodule DungeonCrawl.CLI.Main do
 
   def start_game do
     welcome_message()
+    Shell.prompt("Press enter to continue")
+    hero_choice()
   end
 
   defp welcome_message do
     Shell.info("== Dungeon Crawl ===")
     Shell.info("You awake in a dungeon full of monsters.")
     Shell.info("You need to survive and find the exit.")
+  end
+
+  defp hero_choice do
+    DungeonCrawl.CLI.HeroChoice.start()
   end
 end
