ece3685b58ebe468c85a6dd55d4332085544655c
thebrianemory <the@brianemory.com>
2018-01-29 21:14:00 -0800

    Refactor using base_commands.ex

--- chapter6/dungeon_crawl/lib/dungeon_crawl/cli/hero_choice.ex
+++ chapter6/dungeon_crawl/lib/dungeon_crawl/cli/hero_choice.ex
@@ -1,5 +1,6 @@
 defmodule DungeonCrawl.CLI.HeroChoice do
   alias Mix.Shell.IO, as: Shell
+  import DungeonCrawl.CLI.BaseCommands
 
   def start do
     Shell.cmd("clear")
@@ -16,24 +17,6 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     |> parse_answer
     |> find_hero_by_index.()
     |> confirm_hero
-  end
-
-  def display_options(options) do
-    options
-    |> Enum.with_index(1)
-    |> Enum.each(fn {option, index} -> Shell.info("#{index} - #{option}") end)
-
-    options
-  end
-
-  defp generate_question(options) do
-    options = Enum.join(1..Enum.count(options), ",")
-    "Which one? [#{options}]\n"
-  end
-
-  defp parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
   end
 
   defp confirm_hero(chosen_hero) do
