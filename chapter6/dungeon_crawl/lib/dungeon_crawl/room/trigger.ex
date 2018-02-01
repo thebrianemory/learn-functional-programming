80bd2ffc2332f38e7ba5e60c5824b5c39a79d9f7
thebrianemory <the@brianemory.com>
2018-02-01 09:09:55 -0800

    Fix typo

--- chapter6/dungeon_crawl/lib/dungeon_crawl/room/trigger.ex
+++ chapter6/dungeon_crawl/lib/dungeon_crawl/room/trigger.ex
@@ -2,5 +2,5 @@ defmodule DungeonCrawl.Room.Trigger do
   alias DungeonCrawl.Character
   alias DungeonCrawl.Room.Action
 
-  @callback run(Character.t(), Action.t()) || {Character.t(), atom}
+  @callback run(Character.t(), Action.t()) :: {Character.t(), atom}
 end
