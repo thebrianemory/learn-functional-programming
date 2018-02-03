6fc73c3a0abaad9158f73b81786a991074bea019
thebrianemory <the@brianemory.com>
2018-02-02 18:35:48 -0800

    Update room for chance to encounter an enemy

--- chapter6/dungeon_crawl/lib/dungeon_crawl/room.ex
+++ chapter6/dungeon_crawl/lib/dungeon_crawl/room.ex
@@ -12,6 +12,11 @@ defmodule DungeonCrawl.Room do
         description: "You can see the light of the day. You found the exit!",
         actions: [forward()],
         trigger: Triggers.Exit
+      },
+      %Room{
+        description: "You can see an enemy blocking your path.",
+        actions: [forward()],
+        trigger: Triggers.Enemy
       }
     ]
 end
