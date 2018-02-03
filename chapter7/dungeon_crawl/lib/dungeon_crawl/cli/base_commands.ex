2d15d73615e5ecc02fa3ff666bd92a6860057a88
thebrianemory <the@brianemory.com>
2018-02-03 11:52:38 -0800

    Add controll-flow statements

--- chapter7/dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
+++ chapter7/dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
@@ -20,4 +20,35 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     {option, _} = Integer.parse(answer)
     option - 1
   end
+
+  def ask_for_index(options) do
+    answer =
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt()
+      |> Integer.parse()
+
+    case answer do
+      :error ->
+        display_invalid_option()
+        ask_for_index(options)
+
+      {option, _} ->
+        option - 1
+    end
+  end
+
+  def display_invalid_option do
+    Shell.cmd("clear")
+    Shell.error("Invalid option.")
+    Shell.prompt("Press enter to try again")
+    Shell.cmd("clear")
+  end
+
+  def ask_for_option(options) do
+    index = ask_for_index(options)
+    chosen_option = Enum.at(options, index)
+    chosen_option || (display_invalid_option() && ask_for_option(options))
+  end
 end
